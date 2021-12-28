eeb6529195bf65b8b95cd537a98af9f382875adb
quarkusio <no-reply@quarkus.io>
2021-12-28 10:07:43 +0000

    Initial commit

---
+++ src/test/java/at/htl/NativeReactiveGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package at.htl;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
